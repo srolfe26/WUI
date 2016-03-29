0893277b3ea7d677c47fa89bb05df47c761f8baf
Stephen Rolfe Nielsen <rolfe.nielsen@gmail.com>
2016-03-29 09:18:20 -0600

    Combo Documentation

--- src/js/Form/Combo.js
+++ src/js/Form/Combo.js
@@ -1,19 +1,167 @@
+/**
+ * Wui.Combo
+ * =================================================================================================
+ * (a combination of a select and an autocomplete)
+ *
+ * 
+ * Functionality
+ * -------------
+ *
+ * - Combo requires valueItem and titleItem attributes. 
+ *     - If consuming a `<select>` off the DOM (see below), these values will be set automatically.
+ *     - By default, these will automatically create a template of '<li>{titleItem}</li>'
+ * - Custom templates can be defined for the option list items on the Combo, and follow the rules 
+ *   of the Wui.Template object.
+ * - Multiple selection is not supported at this time
+ * - Arrow button can be removed to make the control appear more like an autocomplete
+ *
+ * 
+ * - Can be asynchronously loaded from a remote data store and search locally
+ *     - Configs: url, [params], autoLoad = true
+ * - Can be asynchronously loaded from a remote data store and search asynchronously
+ *     - Configs: url, [params], searchLocal = false
+ * - Can assume the position and values of a `<select>` element on the DOM
+ *     - Data set in the object definition, OR
+ *     - Data will be created from a select box
+ *         - Combo construction will take the form `new Wui.Combo(<config object>, <select box>)`.
+ *         - Disabled options will have a value of `null`.
+ *         - Options without a value attribute will get the value of the text they contain.
+ *         - Options with a blank value, or no value attribute and no text sub-node, will return
+ *           a value of "".
+ *         - Data will be in the form:
+ *             ```
+ *             {
+ *                 value: '<value attribute, or other as described above>', 
+ *                 label: '<text sub-node of the option tag>'
+ *             }
+ *             ```
+ *
+ * Interactions
+ * ------------
+ *
+ * - Clicking:
+ *     - When the Combo's field doesn't have focus, clicking on the field will open the dropdown
+ *       and select all of the text currently in the field.
+ *     - Clicking the Combo's field when it does have focus merely moves the cursor within the field.
+ *     - Clicking the Combo's arrow button toggles the dropdown open and shut
+ *     - Clicking on a menu item in the option list will select the item, fill the field with the
+ *       selected item, close the drop down, and put the cursor at the end of the field.
+ *     - Clicking away from the Combo will close the option list (if open) and remove focus.
+ * 
+ * - Hovering
+ *     - Hovering over an item in the option list will also place the text of the 'titleItem' in the
+ *       text field, if the item is not selected, when the user hovers out the text field will revert
+ *       to whatever text was in it before.
+ * 
+ * - Typing
+ *     - Tabbing
+ *         - Tabbing into the drop down will select the text in the field, but will not 
+ *           open the dropdown.
+ *         - Tabbing when the field has focus will set the current selection and move away from the
+ *           field to the next tab item.
+ *             - If the option list is open, it will be closed
+ *             - If the field is blank, and there is a blank item in the options, the field will be 
+ *               blanked. Otherwise the field will revert to the currently selected item. Any text in
+ *               the text field from a hover (see 'Hovering' above) will revert to the selected item.
+ *     - Arrow Down
+ *         - If the option list is open, the selection will move within the list, filling the field
+ *           with the 'titleItem' of the selected item. When the selection reaches the bottom of the 
+ *           list an arrow down press will remove selection and focus will be set on the field with
+ *           whatever value was in the field the last time the field had focus. An arrow down from
+ *           focus in the field will select the first item in the options list.
+ *         - If the option list is closed, an arrow up or down will move through the options list, 
+ *           changing the field to the 'titleItem' of the currently selected (but unseen) option, and
+ *           also set the field to the value of that item.
+ *     - Arrow Up
+ *         - Same functionality of arrow down in reverse order
+ *     - Enter
+ *         - If the options list is open, enter key will set the value of the field to the currently 
+ *           selected item in the option list, and close the list.
+ *         - If the options list is closed, enter has no effect.
+ *     - Escape
+ *         - Sets the value of the field back to its previous value, and closes the options list 
+ *           if it's open.
+ *     - Any other typing will cause a search of the options list.
+ *         - Local searching (determined by the searchLocal attribute, default: true) will cause an
+ *           unbounded search of the DOM text of the items in the options list.
+ *         - Remote searching will send requests to the server, and will be at the mercy of the rules
+ *           of the search method on the server.
+ *             - Causes a redraw of the options list
+ *             - Matching text in the DOM elements of the options list will be hilighted, but may not
+ *               necessarily match the rules of the remote search. This is not an error, but care
+ *               should be taken to make sure hilighting is not spotty or nonsensical if/when
+ *               rules mismatch.
+ *             - The search parameter passed to the back-end is the value of the field, and by default
+ *               it's named 'srch'. It can be changed via the `searchArgName` prameter.
+ *             - A minimum number of characters can be set before a remote search will fire so that
+ *               there are not too many search results. This is set with the `minKeys` attribute. The
+ *               default value though is 1.
+ *         - If there are no matching results, by default 'No Results.' will be shown in the options
+ *           list. This message can be changed with the `emptyMsg` attribute.
+ */
 Wui.Combo = function(args){
     $.extend(this, {
+        // Whether to load remote data on instantiation of the Combo (true), or to load 
+        // remote data based on a user's search. Default: false.
         autoLoad:   false,
+        
+        // A CSS class that will be applied to the options list (or dropdown). This class can be
+        // useful for setting a max/min height or width, or for special styling.
         ddCls:      '',
+        
+        // The message to display when a search yields no results, whether local or remote. May be
+        // a plain text string, or HTML formatted.
         emptyMsg:   'No Results.',
+        
+        // The text field for the combo. This field is specified in the constructor of the Combo (as
+        // opposed to the prototype) because it must be a new DOM node for every instance of
+        // the Combo.
         field:      $('<input>',{type:'text'}),
+        
+        // This field is a hidden field containing the value of the combo for use in an HTML form.
+        // This is the field that will get a name and get picked up in a form submit.
+        // TODO: (sn) When inplementing a combo that can take on a <select> item, this field will 
+        // need to be evaluated.
         hiddenField:$('<input>',{type:'hidden'}),
+        
+        // Whether the combo is searchable or not. When this value is false the Combo behaves more
+        // like a select and less like an autocomplete.
         filterField:true,
+        
+        // TODO: Find what this attribute is for. It has no meanining in ths context...
         forceSelect:false,
+        
+        // The minimum number of characters that must be in the field before a search will occur. If
+        // searching against a very large dataset, increasing this number will help reduce the
+        // size of the search results.
         minKeys:    1,
+        
+        // The name of the variable that will contain the search parameters on a remote search.
         searchArgName:'srch',
+        
+        // Tells the combo box to only search the local data rather than perform a remote call. This
+        // can be used where the data is defined locally, or in concert with `autoLoad` where 
+        // remotely loaded data is only searched on the client.
         searchLocal:true,
+        
+        // Determines whether to show the arrow button (that toggles the option list). A false 
+        // value and makes the Combo appear more like an autocomplete field.
         showDD:     true,
+        
+        // The name of the part of the data containing the value that will be shown to the user.
+        // For example: if the data is US states: [{state_id: 1, state_name:"Alabama"}, ...]
+        // the titleItem will be 'state_name'. titleItem is REQUIRED.
         titleItem:  null,
+        
+        // The value part of the data that will be used/stored by the program.
+        // For example: if the data is US states: [{state_id: 1, state_name:"Alabama"}, ...]
+        // the valueItem will be 'state_id'. valueItem is REQUIRED.
         valueItem:  null
-    },args,{
+    }, args, {
+        // Determies whether multiple selections can be made. At this time, on this control, 
+        // multiselect is not available.
+        // TODO: (sn) When adding the ability to consume <select> fields, if the select is a multiple,
+        // bring in the Wui.Multiple
         multiSelect:false
     }); 
 
